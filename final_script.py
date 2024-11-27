67485ff98d18c17093a24382888c8c9eada86937
Surendra030 <surendrababun030@gmail.com>
2024-11-27 17:20:45 +0530

    none..

--- final_script.py
+++ final_script.py
@@ -71,7 +71,7 @@ def main(drive_url,snippet):
     downloaded_file_name = download_file_from_drive(drive_url)
     print(f"Downloaded file: {downloaded_file_name}")
     if downloaded_file_name:
-        Extension = downloaded_file_name.split(".")[0]
+        Extension = downloaded_file_name.split(".")[-1]
         downloaded_file_name = f'{snippet["link"]}.{Extension}'
 
         # Step 2: Extract audio streams from the downloaded file
