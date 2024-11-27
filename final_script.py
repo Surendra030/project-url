d5463c01fedd959b72a9c59712f03e533895b8c4
Surendra030 <surendrababun030@gmail.com>
2024-11-27 16:34:44 +0530

    none..

--- final_script.py
+++ final_script.py
@@ -68,7 +68,7 @@ def upload_to_mega(local_file_path, mega_folder_path):
 def main(drive_url,snippet):
     # Step 1: Download the file from Google Drive
     downloaded_file_name = download_file_from_drive(drive_url)
-    
+    print(downloaded_file_name)
     if downloaded_file_name:
         print(f"Downloaded file: {downloaded_file_name}")
         # Step 2: Extract audio streams from the downloaded file
